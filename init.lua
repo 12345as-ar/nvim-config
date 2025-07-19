b16b79d442a654b6b2f0f418f547fc3e139cbcfc
12345a-ar <oliverdev.pfister@gmail.com>
2025-07-19 15:12:34 +0200

    my stuff

---
+++ init.lua
@@ -0,0 +1,22 @@
+print("ooofficer balls")
+
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus" 
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.keymap.set("n", "<enter>", "o<esc>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+    	vim.highlight.on_yank()
+    end,
+})
